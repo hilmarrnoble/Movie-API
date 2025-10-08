fcd882f39b698fc3d2ceb6af6f1e255efec7dcab
Hilmar Noble <hilmarrnoble@gmail.com>
2025-10-09 00:12:21 +0200

    Prepare for Heroku deployment

--- index.js
+++ index.js
@@ -20,5 +20,5 @@ app.use(cors()); // To handle cross-origin requests
 app.use('/api/users', userRoutes); // Mount user-related routes
 
 // Start the server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
