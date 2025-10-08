07c811ca14288ed08a6262dda9cb3ec006ca0e4e
Hilmar Noble <hilmarrnoble@gmail.com>
2025-10-08 22:53:01 +0200

    Temporarily remove JWT authentication from /movies endpoint

--- routes/movies.js
+++ routes/movies.js
@@ -1,13 +1,43 @@
-const express = require('express');
+// routes/movies.js
+import express from "express";
+import * as movieController from "../controllers/movieController.js";
+import { authenticateToken } from "../middleware/authMiddleware.js";
+
 const router = express.Router();
-const movieController = require('../controllers/movieControllers');
-const authenticateToken = require('../middleware/auth');
 
-router.get('/', movieController.getAllMovies);
-router.get('/title/:title', movieController.getMovieByTitle);
-router.get('/genres/:name', movieController.getGenreDescription);
-router.get('/directors/:name', movieController.getDirector);
-router.post('/', authenticateToken, movieController.createMovie);
+/**
+ * @route GET /api/movies
+ * @desc Get all movies
+ * @access Public (temporarily)
+ */
+router.get("/", movieController.getAllMovies);
 
-module.exports = router;
+/**
+ * @route GET /api/movies/:title
+ * @desc Get a single movie by title
+ * @access Protected
+ */
+router.get("/:title", authenticateToken, movieController.getMovieByTitle);
 
+/**
+ * @route POST /api/movies
+ * @desc Add a new movie
+ * @access Protected
+ */
+router.post("/", authenticateToken, movieController.createMovie);
+
+/**
+ * @route PUT /api/movies/:title
+ * @desc Update a movie by title
+ * @access Protected
+ */
+router.put("/:title", authenticateToken, movieController.updateMovie);
+
+/**
+ * @route DELETE /api/movies/:title
+ * @desc Delete a movie by title
+ * @access Protected
+ */
+router.delete("/:title", authenticateToken, movieController.deleteMovie);
+
+export default router;
