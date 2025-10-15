f2d59f8de885acc00f8b467c3bc1add32637b360
Hilmar Noble <hilmarrnoble@gmail.com>
2025-10-15 20:02:22 +0200

    updated controllers & routes
    -updated userControllers.js
    -updated movies.js & users.js

--- routes/users.js
+++ routes/users.js
@@ -5,8 +5,15 @@ const userController = require('../controllers/userControllers');
 
 const router = express.Router();
 
-// Useful endpoints; keep auth/login on /auth
 router.get('/', authenticateToken, userController.getAllUsers);
 router.get('/me', authenticateToken, userController.getMe);
+
+// Profile update / delete
+router.put('/me', authenticateToken, userController.updateMe);
+router.delete('/me', authenticateToken, userController.deleteMe);
+
+// Favorites
+router.post('/me/favorites/:movieId', authenticateToken, userController.addFavorite);
+router.delete('/me/favorites/:movieId', authenticateToken, userController.removeFavorite);
 
 module.exports = router;
